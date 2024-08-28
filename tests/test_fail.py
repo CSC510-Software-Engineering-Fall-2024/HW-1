7a9e79514d3d67a22840464da170b1a2a357fa0d
Dharani-Guda <86072402+Dharani-Guda@users.noreply.github.com>
2024-08-28 21:14:47 +0000

    test file modified

--- tests/test_fail.py
+++ tests/test_fail.py
@@ -5,4 +5,4 @@ sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 from hw1 import divide
 
 def divide_pass():
-    assert divide(4, 2) == 2
+    assert divide(4, 5) == 2
