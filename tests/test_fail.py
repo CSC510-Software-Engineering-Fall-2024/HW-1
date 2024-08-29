e8b65526cfbc9ab0a5016ebabd3b7c8a69e6eff5
Dharani-Guda <86072402+Dharani-Guda@users.noreply.github.com>
2024-08-29 00:20:53 +0000

    fixed test cases

--- tests/test_fail.py
+++ tests/test_fail.py
@@ -4,5 +4,5 @@ sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
 from hw1 import divide
 
-def divide_pass():
+def test_fail():
     assert divide(4, 5) == 2
--- tests/test_pass.py
+++ tests/test_pass.py
@@ -4,5 +4,5 @@ sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
 from hw1 import divide
 
-def divide_pass():
+def test_pass():
     assert divide(5, 1) == 5
